938d427f883c66b106b94487eb58fe0c101fc1a7
Victor Caceres <caceres.vc95@gmail.com>
2020-01-24 12:58:45 -0400

    create redux

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Main } from './src/components';
+import configureStore from './src/store';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const store = configureStore();
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <Main/>
+      </Provider>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
