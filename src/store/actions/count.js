216e046c1443168379616f31a2e9bac7a667d7ad
Victor Caceres <caceres.vc95@gmail.com>
2020-01-24 18:47:51 -0400

    change

--- src/store/actions/count.js
+++ src/store/actions/count.js
@@ -1,24 +1,12 @@
 import { INCREMENT_COUNT, DECREMENT_COUNT } from '../actions-type';
 
-export const incrementCount = count => dispatch => {
-  setTimeout(() => {
-    dispatch(getPayload(INCREMENT_COUNT, count + 1));
-  }, 1500);
+export const incrementCount = (count) => {
+  return getPayload(INCREMENT_COUNT, count + 1);
 }
 
-export const decrementCount = count => dispatch => {
-  setTimeout(() => {
-    dispatch(getPayload(DECREMENT_COUNT, count - 1));
-  }, 1500);
+export const decrementCount = (count) => {
+  return getPayload(DECREMENT_COUNT, count - 1);
 }
-
-// export const incrementCount = (count) => {
-//   return getPayload(INCREMENT_COUNT, count + 1);
-// }
-
-// export const decrementCount = (count) => {
-//   return getPayload(DECREMENT_COUNT, count - 1);
-// }
 
 const getPayload = (type, payload) => {
   return {
