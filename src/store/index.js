9d7f11a562fd30aeae3c7150a7c9714d376ce303
Victor Caceres <caceres.vc95@gmail.com>
2020-01-24 16:53:34 -0400

    add thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { countReducer } from './reducers';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
     count: countReducer
 });
 
 const configureStore = () => {
-    return createStore(rootReducer);
+    return createStore(rootReducer, applyMiddleware(thunk));
 }
 
 export default configureStore;
